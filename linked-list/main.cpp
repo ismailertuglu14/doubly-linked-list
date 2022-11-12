f7663fb20888f2b60240385b554ebbbb06a57080
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-11-13 01:14:18 +0300

    Initial Commit

---
+++ linked-list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linked-list
+//
+//  Created by İsmail Ertuğlu on 13.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
